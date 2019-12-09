a51f07f64d8f634c76b969727ce5ade89a0dbf83
Martin <github@diemattels.at>
2019-12-09 17:33:44 +0100

    Update monitor_gz.sh

--- Ubuntu gz Scripts/monitor_gz.sh
+++ Ubuntu gz Scripts/monitor_gz.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo 'strace static gz files'
+echo 'run `sudo service nginx reload` in a new terminal'
+echo 'strace can only trace children created after attached to the master process'
+
 strace -e trace=open -p `pidof nginx | sed -e 's/ /,/g'` 2>&1 | grep .gz
